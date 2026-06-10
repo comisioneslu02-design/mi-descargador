a619ce891fb1394e2e57c7be83601030a72af61f
Luka <luka@example.com>
2026-06-09 20:19:50 -0400

    Initial commit of yt-dlp downloader

--- test-format.js
+++
@@ -1,9 +0,0 @@
-const ytdl = require('ytdl-core');
-
-async function testD() {
-    const info = await ytdl.getInfo('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-    const formats = ytdl.filterFormats(info.formats, 'audioandvideo');
-    console.log('Combined formats:', formats.length);
-    if(formats.length > 0) console.log('First combined format:', formats[0].mimeType);
-}
-testD();
